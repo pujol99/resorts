320acf0761c4bae83da3a5a112bd07fbf17a1c5d
pujol99 <alexpujolcomet8@gmail.com>
2022-02-22 23:24:17 +0100

    Nice welcome

--- src/store/modules/labels.js
+++ src/store/modules/labels.js
@@ -1,13 +1,12 @@
 export const labels = {
-    continue: ["Continue", "Avança"],
     /**
      *
      *  Welcome
      *
      */
-    welcome_title: ["TFG", "TFG"],
+    welcome_title: ["Worlds", "Mons"],
     welcome_body: [
-        "Game for raising awarness on social media addiction ",
-        "Joc per conscienciar sobre l'addicció a les xarxes socials",
+        "Exploration of planets with different airplanes",
+        "Exploration of planets with different airplanes",
     ],
 };
